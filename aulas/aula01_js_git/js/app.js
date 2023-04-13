5a1166632920629477b92bd6c5ce7b7879bcca51
George Chaves <george_hfc@yahoo.com.br>
2023-04-13 13:29:06 +0100

    stuffing js

--- aulas/aula01_js_git/js/app.js
+++ aulas/aula01_js_git/js/app.js
@@ -1,4 +1,34 @@
+function holaMundo() {
+  console.log("¡Hola Mundo! 🇦🇷");
+}
+
+function aloMundo() {
+  aloBrasil = document.getElementById("app");
+  aloBrasil.innerHTML += "<br><div style='text-align:center;'><strong>Olá Mundo!<strong> 🇧🇷</div><br>"
+}
+
 window.onload = function()
 {
-  console.log(document.querySelectorAll('span'));
+  document.onclick = function() {
+    holaMundo();
+  };
+
+  aloMundo();
+
+  var objHello = document.getElementById("hello-kraken");
+  objHello.innerHTML = "Hello World! 🇺🇸";
+
+  objHello = document.getElementsByTagName("strong")[0];
+  console.log(objHello);
+  objHello.onmouseover = function() {
+    objHello.style.backgroundColor = "white";
+    objHello.innerHTML = "Ciao Mondo 🇮🇹";
+  }
+  objHello.onmouseout = function() {
+    objHello.style.backgroundColor = "";
+    objHello.innerHTML = "George Chaves";
+  }
+
+  // USE: querySelector()
+  // USE: alert()
 }
