0344bbd4dc51480647fe586cfb882383fa78b5b6
George Chaves <george_hfc@yahoo.com.br>
2023-05-17 01:04:55 +0100

    code clean up

--- aulas/modulo03_funcoes/src/form.js
+++ aulas/modulo03_funcoes/src/form.js
@@ -8,7 +8,7 @@ const answers = document.querySelector(".answers");
 // CALLBACKS
 const handleSubmit = (onSubmit, onError, self) => {
   if (passInput.value.length < 6) {
-    nameInput.focus();
+    passInput.focus();
     return onError();
   }
 
@@ -29,71 +29,15 @@ const submit = () => {
 
     let table = document.querySelector('table');
     let row = table.insertRow();
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
 
-    cell1.innerHTML = `${nameInput.value}`;
-    if (!document.querySelector('.agreement input').checked) {
-      cell1.innerHTML += ' ☠️';
-    }
-    cell2.innerHTML = `${document.querySelector('input[name=fav_language]:checked').value}`;
-    cell3.innerHTML = `${selectedCity.value}`;
+    (document.querySelector('.agreement input').checked) ? row.insertCell().innerHTML = `${nameInput.value}` : row.insertCell().innerHTML = `${nameInput.value} ☠️`;
+    (document.querySelector('input[name=fav_language]:checked')) ? row.insertCell().innerHTML = `${document.querySelector('input[name=fav_language]:checked').value}` : row.insertCell().innerHTML = '-';
+    (selectedCity.value) ? row.insertCell().innerHTML = `${selectedCity.value}` : row.insertCell().innerHTML = '-';
   }, 1000);
 };
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("Handling Submit:");
+  // console.log("Handling Submit:");
   handleSubmit(submit, error, this)
 });
-
-// PROMISES
-const funcPromise = (condition) => {
-  return new Promise((resolve, reject) => {
-    // console.log('New Promise Returned:');
-    if (!condition) {
-      return reject();
-    }
-    resolve();
-  });
-};
-
-funcPromise(false)
-  .then(() => {
-    // console.log('Promise ok');
-  })
-  .catch(() => {
-    // console.log('Promise error');
-  });
-
-// ASYNC PROMISE
-const funcAsyncPromise = async (condition) => {
-  // console.log('New (Async) Promise Returned:');
-  if(!condition) {
-    throw new Error('Condition is set to false.');
-  }
-  return true;
-};
-
-funcAsyncPromise(true)
-  .then(() => {
-    // console.log('(Async) Promise ok');
-  })
-  .catch((error) => {
-    // console.log('(Async) Promise error');
-    // console.log(error);
-  });
-
-// LOOPS
-const numbers = [1,2,3,4,5];
-
-const numberObjects = numbers.map((current, index, array) => {
-  return {
-    number: current,
-    index: index,
-    array: array };
-});
-
-// console.log("Loops: ");
-// console.log(numberObjects);
--- aulas/modulo03_funcoes/src/lecture.js
+++ aulas/modulo03_funcoes/src/lecture.js
@@ -49,3 +49,53 @@ loop(1);
   qwe.name("qwe");
   qwe.nameArrow();
 })()
+
+// PROMISES
+const funcPromise = (condition) => {
+  return new Promise((resolve, reject) => {
+    // console.log('New Promise Returned:');
+    if (!condition) {
+      return reject();
+    }
+    resolve();
+  });
+};
+
+funcPromise(false)
+  .then(() => {
+    // console.log('Promise ok');
+  })
+  .catch(() => {
+    // console.log('Promise error');
+  });
+
+// ASYNC PROMISE
+const funcAsyncPromise = async (condition) => {
+  // console.log('New (Async) Promise Returned:');
+  if(!condition) {
+    throw new Error('Condition is set to false.');
+  }
+  return true;
+};
+
+funcAsyncPromise(true)
+  .then(() => {
+    // console.log('(Async) Promise ok');
+  })
+  .catch((error) => {
+    // console.log('(Async) Promise error');
+    // console.log(error);
+  });
+
+// LOOPS
+const numbers = [1,2,3,4,5];
+
+const numberObjects = numbers.map((current, index, array) => {
+  return {
+    number: current,
+    index: index,
+    array: array };
+});
+
+// console.log("Loops: ");
+// console.log(numberObjects);
